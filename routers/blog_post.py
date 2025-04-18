370f5861b4806d6fa82a756afebab793483526c5
Surendrakumar Deshpande <sdesh02@bhnetwork.com>
2025-04-18 23:22:35 +0530

    adding body for the post call

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,10 +1,23 @@
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/blog",
     tags=["blog"]
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
+#body, path param, query param -- sequence of function parameters
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int , version: int = 1):
+    return { 
+        'id' : id,
+        'data' : blog,
+        'version': version
+        }
